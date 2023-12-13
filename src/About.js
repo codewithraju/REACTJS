5a69ac75446c5f8fa86f23b78c936ea01feef813
codewithraju <rajuroy.bits@gmail.com>
2023-12-13 23:21:22 +0530

    This is a new commit for what I originally planned to be an amendmend

---
+++ src/About.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+ function Myraju () {
+return <div>
+    <h1>welcome to my react page</h1>
+    <ol>
+        <ul>Raju</ul>
+        <ul>sayrabh</ul>
+        <ul>Rambabu</ul>
+        <ul>moihit</ul>
+    </ol>
+</div>
+
+};
+
+export default Myraju
